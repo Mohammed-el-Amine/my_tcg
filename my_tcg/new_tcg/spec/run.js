31d99cfb7274f1285e54a92482550aab3ca55141
amine <mohammed-el-amine.djellal@epitech.eu>
2023-02-21 17:03:21 +0100

    [ADD] contents from zip, need it for start projet, add a 'start' script , and a text messaage before the test start

---
+++ my_tcg/new_tcg/spec/run.js
@@ -0,0 +1,9 @@
+import Jasmine from 'jasmine'
+import specReporter from 'jasmine-spec-reporter'
+
+var jasmine = new Jasmine()
+jasmine.configureDefaultReporter({print: () => {}})
+jasmine.addReporter(new specReporter())
+jasmine.loadConfigFile('spec/support/jasmine.json')
+console.log('Début du test avec Jasmine\n')
+jasmine.execute()
